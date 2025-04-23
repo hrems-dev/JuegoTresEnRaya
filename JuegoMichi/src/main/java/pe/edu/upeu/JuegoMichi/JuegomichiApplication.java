49e197e75f36f7afc4e7687ae9093aa10770398f
hrems-dev <hrems077@gmail.com>
2025-04-22 21:41:07 -0500

    mi juego principio

---
+++ JuegoMichi/src/main/java/pe/edu/upeu/JuegoMichi/JuegomichiApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.JuegoMichi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuegomichiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuegomichiApplication.class, args);
+	}
+
+}
---
+++ JuegoMichi/src/test/java/pe/edu/upeu/JuegoMichi/JuegomichiApplicationTests.java
@@ -0,0 +1,14 @@
+package pe.edu.upeu.JuegoMichi;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuegomichiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
