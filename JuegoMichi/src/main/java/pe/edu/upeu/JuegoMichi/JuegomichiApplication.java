033d539c05a7eebfbec310b01b73b6b6c4aaecf6
hrems-dev <hrems077@gmail.com>
2025-04-24 21:06:07 -0500

    ultima config compu

--- JuegoMichi/src/main/java/pe/edu/upeu/JuegoMichi/JuegomichiApplication.java
+++ JuegoMichi/src/main/java/pe/edu/upeu/JuegoMichi/JuegomichiApplication.java
@@ -3,11 +3,13 @@ package pe.edu.upeu.JuegoMichi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static javafx.application.Application.launch;
+
 @SpringBootApplication
 public class JuegomichiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JuegomichiApplication.class, args);
+		launch(args);
 	}
 
 }
---
+++ JuegoMichi/src/main/java/pe/edu/upeu/JuegoMichi/modelo/Jgars.java
@@ -0,0 +1,12 @@
+package pe.edu.upeu.JuegoMichi.modelo;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Jgars {
+    ArrayList<String> jugador1=new ArrayList<>();
+    ArrayList<String> jugador2=new ArrayList<>();
+    int valores[][]=new int[3][3];
+    //valores
+    valores[0][0]=1;
+}
